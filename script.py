23143e902e526eb302a0b7f4a94613273d24220a
Devilal panchal <devilalpanchal551@gmail.com>
2024-02-26 22:28:01 +0530

    add calender

--- script.py
+++ script.py
@@ -1,4 +1,20 @@
- a =5
- b =10
- c = a+b
- print(c)
+
+# For add two number
+
+let = 51
+var = 25
+number = let + var
+print(let + var) 
+print(let - var)
+print(let * var)
+print(let / var)
+print(let % var)
+print('Devilal panchal roll number is',let)
+
+import calendar
+yy = 2024  # year
+mm = 2     # month
+# display the calendar 
+print(calendar.month(yy,mm)) 
+
+
